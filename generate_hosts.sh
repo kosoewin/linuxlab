480ff4b5e08983fffd833115b6fcbd8f358a683c
kosoewin <53328736+kosoewin@users.noreply.github.com>
2023-06-09 19:14:06 +0630

    Update generate_hosts.sh

--- generate_hosts.sh
+++ generate_hosts.sh
@@ -2,7 +2,7 @@
 
 # Update /etc/hosts file
 sudo bash -c 'cat >> /etc/hosts' << EOF
-192.168.101.10	centos_lab.local  centos_lab
-192.168.101.20	ubuntu_lab.local  ubuntu_lab
+192.168.101.10	centoslab.local  centoslab
+192.168.101.20	ubuntulab.local  ubuntulab
 
 EOF
